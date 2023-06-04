20935e5d7b4c96a0381c69d9dcffc9e7acab407b
Victor Mendez <victor.mmc.pl@hotmail.com>
2023-06-04 11:30:58 -0600

    ci: Defined Toggle hook
    Also configures project to use SVG elements as React components

---
+++ src/hooks/toggle.test.tsx
@@ -0,0 +1,60 @@
+import { ReactComponent as ArrowDown } from '../assets/svg/keyboard-arrow/keyboard-arrow-down.svg'
+import { ReactComponent as Up } from '../assets/svg/keyboard-arrow/keyboard-arrow-up.svg'
+
+import { act, renderHook } from "@testing-library/react"
+import useToggle from "./toggle"
+
+describe('Toggle Hook', () => { 
+    test('initial state should be false', () => { 
+        const { result } = renderHook(() => useToggle({ iconTrue: Up, iconFalse: ArrowDown }))
+
+        expect(result.current.toggle).toBe(false)
+    })
+
+    test('initial icon should be ArrowDown', () => { 
+        const { result } = renderHook(() => useToggle({ iconTrue: Up, iconFalse: ArrowDown }))
+
+    expect(result.current.Icon).toEqual(ArrowDown)
+    })
+
+    test('initial toggle state should be true when setToggle is called', () => { 
+        const { result } = renderHook(() => useToggle({ iconTrue: Up, iconFalse: ArrowDown }))
+
+        act(() => {
+            result.current.setToggle(true)
+        })
+
+        expect(result.current.toggle).toBe(true)
+    })
+
+    test('toggle state should change when toggleState is called', () => { 
+        const { result } = renderHook(() => useToggle({ iconTrue: Up, iconFalse: ArrowDown }))
+        const initialState = result.current.toggle
+
+        act(() => {
+            result.current.toggleState()
+        })
+
+        expect(result.current.toggle).toBe(!initialState)
+    })
+
+    test('icon should be ArrowDown when toggle is false', () => { 
+        const { result } = renderHook(() => useToggle({ iconTrue: Up, iconFalse: ArrowDown }))
+
+        act(() => {
+            result.current.setToggle(false)
+        })
+
+        expect(result.current.Icon).toEqual(ArrowDown)
+    })
+
+    test('icon should be Up when toggle is true', () => { 
+        const { result } = renderHook(() => useToggle({ iconTrue: Up, iconFalse: ArrowDown }))
+
+        act(() => {
+            result.current.setToggle(true)
+        })
+
+        expect(result.current.Icon).toEqual(Up)
+    })
+ })
---
+++ src/hooks/toggle.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+interface ToggleProps {
+    iconTrue: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+    iconFalse: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+}
+
+interface ToggleReturn {
+    toggle: boolean
+    setToggle: React.Dispatch<React.SetStateAction<boolean>>
+    toggleState: () => void
+    Icon: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+  }
+
+/**
+ * Toggle hook
+ * @description Hook to use in toggle components, to control display of elements
+ * @param iconTrue Icon to use when toggle state is true
+ * @param iconFalse Icon to use when toggle state is false
+ * @returns {object}
+ * @example useToggle({ iconTrue: <ArrowUp/>, iconFalse: <ArrowDown/> })
+ */
+function useToggle({ iconTrue, iconFalse }: ToggleProps): ToggleReturn {
+    const [toggle, setToggle] = useState(false)
+    const Icon = toggle ? iconTrue : iconFalse
+
+    function toggleState() {
+        setToggle(!toggle)
+    }
+    
+    return { toggle, setToggle, toggleState, Icon }
+}
+
+export default useToggle
