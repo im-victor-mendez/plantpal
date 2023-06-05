a695a1784b76962e7b70a0326782b71d25cc011e
Victor Mendez <victor.mmc.pl@hotmail.com>
2023-06-04 19:19:20 -0600

    ci: Defined Input element and Input Toggle

---
+++ src/components/Input/Input.test.tsx
@@ -0,0 +1,31 @@
+import { screen, render } from '@testing-library/react'
+import Input, { InputToggle as Toggle } from './Input'
+
+describe('Input', () => {
+    describe('Default', () => {
+        test('should display placeholder', () => {
+            const placeholder = 'Placeholder'
+
+            render(<Input placeholder={placeholder} setValue={function (): void {
+                throw new Error('Function not implemented.')
+            } }/>)
+
+            const element = screen.getByPlaceholderText(placeholder)
+            expect(element).toBeDefined()
+        })
+    })
+
+    describe('Toggle', () => {
+        test('should display name', () => {
+            const name = 'Toggle Name'
+
+            render(<Toggle name={name} values={[]} setValue={function (): void {
+                throw new Error('Function not implemented.')
+            } }/>)
+
+            const element = screen.getByText(name)
+
+            expect(element).toBeDefined()
+        })
+    })
+})
---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,74 @@
+import useToggle from '../../hooks/toggle'
+import './Input.scss'
+import { ChangeEvent, Dispatch, SetStateAction } from 'react'
+import { ReactComponent as ArrowDown } from '../../assets/svg/keyboard-arrow/keyboard-arrow-down.svg'
+import { ReactComponent as ArrowUp } from '../../assets/svg/keyboard-arrow/keyboard-arrow-up.svg'
+
+interface InputProps {
+    placeholder: string,
+    setValue: Dispatch<SetStateAction<string>>
+}
+
+/**
+ * Input
+ * @description Returns custom input element
+ * @param placeholder Placeholder to display
+ * @param setValue Set state function to handle on change value
+ * @returns {React.JSX.Element}
+ * @example <Input placeholder={`Name`} setValue={setName} />
+ */
+function Input({ placeholder, setValue }: InputProps): React.JSX.Element {
+    /**
+     * On change
+     * @description Handles change value in setValue parent prop
+     * @param event Element Changed
+     */
+    function onChange(event: ChangeEvent<HTMLInputElement>): void {
+        const value = event.target.value
+        setValue(value)
+    }
+
+    return (
+        <input
+            type="text"
+            placeholder={placeholder}
+            className='input input-element'
+            data-testid='input'
+            onChange={onChange}
+        />
+    )
+}
+
+interface ToggleProps {
+    name: string
+    values: Array<object>
+    setValue: Dispatch<SetStateAction<any>>
+}
+
+/**
+ * Input Toggle
+ * @description Returns Toggle of array values
+ * @param name Name of Input
+ * @param values Array of objects to display values
+ * @param setValue Set state function to handle value into parent state
+ * @returns {React.JSX.Element}
+ * @example <InputToggle name={`Friends to gift`} values={friends} setValue={setFriends} />
+ */
+export function InputToggle({ name, values, setValue }: ToggleProps): React.JSX.Element {
+    const { toggle, Icon, toggleState } = useToggle({ iconTrue: ArrowUp, iconFalse: ArrowDown })
+
+    const toggleActive = toggle ? 'active' : ''
+
+    return (
+        <div className={`input toggle ${toggleActive}`}>
+            <div className='top'>
+                <p className='name'>{name}</p>
+                <Icon onClick={toggleState}/>
+            </div>
+            {toggle && <div className='values'>
+            </div>}
+        </div>
+    )
+}
+
+export default Input
