81c2602d7b50d324d832814db6f8d8e03b64f22d
Victor Mendez <victor.mmc.pl@hotmail.com>
2023-06-06 14:44:43 -0600

    chore: Implemented path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +15,18 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom"
+  },
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@firebase': path.resolve(__dirname, './src/firebase'),
+      '@functions': path.resolve(__dirname, './src/functions'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@layouts': path.resolve(__dirname, './src/layouts'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@sass': path.resolve(__dirname, './src/sass'),
+      '@store': path.resolve(__dirname, './src/store')
+    }
   }
 })
